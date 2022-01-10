61984fc9672845396a4f7a565c688dd52be30ca3
Andrii <ramp8a799@gmail.com>
2022-01-10 18:58:46 +0200

    build fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": ["plugin:sonarjs/recommended"],//"eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "rules": {
-        "semi":"error"
-    },
-    "plugins": ["sonarjs"],
-
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:sonarjs/recommended"], //"eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  rules: {
+    semi: "error",
+  },
+  plugins: ["sonarjs"],
 };
